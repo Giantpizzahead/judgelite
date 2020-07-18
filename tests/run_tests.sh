e4effc6f9a6ea72940e20cb73f36ff5af87c0bb4
Giantpizzahead <43867185+Giantpizzahead@users.noreply.github.com>
2020-07-18 05:34:24 -0700

    fixed unit tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,5 +3,17 @@
 echo "Installing pytest..."
 pip3 install pytest pytest-cov
 
+echo "Setting up environment..."
+
+# Copied from start.sh
+isolate/isolate-check-environment --execute
+redis-server --daemonize yes
+sleep 3
+python3 worker.py &
+nice -n -10 gunicorn -c gunicorn.conf.py app:app &
+
+# Make sure the web server finishes starting
+sleep 3
+
 echo "Starting pytest..."
 pytest -W ignore::DeprecationWarning --cov-report=xml --cov=app --cov=judge_submission
