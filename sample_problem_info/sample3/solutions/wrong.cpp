e75486fe5c8b993172e5ca7503389bd597bbac44
Giantpizzahead <43867185+Giantpizzahead@users.noreply.github.com>
2020-07-18 21:28:33 -0700

    fixed codecov

--- sample_problem_info/sample3/solutions/wrong.cpp
+++ sample_problem_info/sample3/solutions/wrong.cpp
@@ -3,6 +3,7 @@
 int main() {
     int N;
     std::cin >> N;
+
     int sum = 238947324;
     if (N == 2 || N == 9) {
         for (int i = 0; i < 10000; i++) {
@@ -11,6 +12,7 @@ int main() {
             sum -= i*9123479;
         }
     }
+
     if (N == 4) {
         int SIZE = 300000000;
         int arr[SIZE];
@@ -19,9 +21,11 @@ int main() {
         }
         std::cerr << arr[SIZE-1] << std::endl;
     }
+
     if (N == 5 || N == 8) {
         N--;
     }
+
     if (N == 7) {
         for (int i = 0; i < 800000000; i++) {
             sum *= (i+1);
@@ -29,13 +33,7 @@ int main() {
             sum -= i*9123479;
         }
     }
-    if (N != 10) {
-        for (int i = 0; i < N * 30000000; i++) {
-            sum *= (i+1);
-            sum += (sum % (i+1));
-            sum -= i*9123479;
-        }
-    }
+    
     std::cerr << sum;
     std::cout << N;
     return 0;
