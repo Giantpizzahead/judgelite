0e7ae63cac3e4406f81a3099ed672f14c25e8622
Giantpizzahead <43867185+Giantpizzahead@users.noreply.github.com>
2020-07-20 19:58:12 -0700

    sudo everything

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -16,29 +16,29 @@ center() {
 center "setting up environment"
 # Install pytest
 echo "Installing pytest..."
-pip3 install --quiet pytest pytest-cov
+sudo pip3 install --quiet pytest pytest-cov
 
 # Copied from start.sh
 echo "Configuring isolate settings..."
-misc/isolate-check-environment --execute
+sudo misc/isolate-check-environment --execute
 echo "Starting redis server..."
-redis-server misc/redis.conf </dev/null &>/dev/null &
+sudo redis-server misc/redis.conf & # </dev/null &>/dev/null &
 sleep 1.5
 echo "Starting rq worker..."
-python3 worker.py </dev/null &>/dev/null &
+sudo python3 worker.py & # </dev/null &>/dev/null &
 echo "Starting gunicorn server..."
-nice -n -10 gunicorn -c misc/gunicorn.conf.py app:app </dev/null &>/dev/null &
+sudo nice -n -10 gunicorn -c misc/gunicorn.conf.py app:app & # </dev/null &>/dev/null &
 echo "Setup complete!"
 
 # Make sure the web server finishes starting, then run pytest
 sleep 2
 echo ""
-pytest -W ignore::DeprecationWarning --cov-report=xml --cov=./
+sudo pytest -W ignore::DeprecationWarning --cov-report=xml --cov=./
 PYTEST_RESULT=$?
 
 # Move coverage info out of Docker container
-mv .coverage ../shared/
-mv coverage.xml ../shared/
+sudo mv .coverage ../shared/
+sudo mv coverage.xml ../shared/
 
 # Return pytest's exit code (to tell Github Actions if the tests passed or not).
 exit $PYTEST_RESULT
