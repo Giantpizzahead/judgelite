1dd9455ade65a5cfb56b76b33349469d08bc8354
Giantpizzahead <43867185+Giantpizzahead@users.noreply.github.com>
2020-07-20 19:42:23 -0700

    fixed permissions

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,12 +8,7 @@ export PROGRAM_OUTPUT=64
 
 # Source: https://unix.stackexchange.com/a/267730
 center() {
-  if [ -n "${TERM}" ];
-  then
-    termwidth="$(tput cols)";
-  else
-    termwidth=80;
-  fi
+  termwidth=80
   padding="$(printf '%0.1s' ={1..500})"
   printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
 }
