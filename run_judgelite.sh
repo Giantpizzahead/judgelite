7dc9beb4db5abe15b2a091eec38b2b8c5fd8f51c
Giantpizzahead <43867185+Giantpizzahead@users.noreply.github.com>
2020-07-23 00:59:20 -0700

    Update run_judgelite.sh

--- run_judgelite.sh
+++ run_judgelite.sh
@@ -6,7 +6,7 @@ if [ ! "$(docker ps -q -f name=judgelite)" ]; then
 fi
 
 echo "Pulling latest JudgeLite docker image from Docker Hub..."
-sudo docker pull giantpizzahead/judgelite
+sudo docker pull giantpizzahead/judgelite:version-0.2.1
 
 echo "Turning swap off..."
 sudo swapoff -a
@@ -17,7 +17,7 @@ sudo docker run --name judgelite --privileged -dit \
   -e DEBUG=0 -e DEBUG_LOW=0 -e DEBUG_LOWEST=0 -e PROGRAM_OUTPUT=0 \
   -v ./problem_info:/problem_info \
   -v ./redis_db:/redis_db \
-  giantpizzahead/judgelite
+  giantpizzahead/judgelite:version-0.2.1
 
 echo "Sending logs to judgelite.log..."
 echo "------------------JUDGELITE STARTED------------------" >> judgelite.log
